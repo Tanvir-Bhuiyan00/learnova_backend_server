56f661d38db55915e187d6109ee0859c04b74d9c
Tanvir Bhuiyan <tanvirbhuiyan2001@gmail.com>
2026-07-14 05:58:01 +0600

    refactor environment variable management and update Prisma configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 const bootstrap = () => {
   try {
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(
-        `Server is running on http://localhost:${process.env.PORT || 5000}`,
-      );
+    app.listen(envVars.PORT, () => {
+      console.log(`Server is running on http://localhost:${envVars.PORT}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error);
