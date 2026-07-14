3d2de04d0261842bf2da926eebd6cd648e1811dd
Tanvir Bhuiyan <tanvirbhuiyan2001@gmail.com>
2026-07-14 23:17:46 +0600

    update Cloudinary public_id and folder structure for file uploads

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -45,8 +45,8 @@ export const uploadFileToCloudinary = async (
       .upload_stream(
         {
           resource_type: "auto",
-          public_id: `ph-healthcare/${folder}/${uniqueName}`,
-          folder: `ph-healthcare/${folder}`,
+          public_id: `learnova/${folder}/${uniqueName}`,
+          folder: `learnova/${folder}`,
         },
         (error, result) => {
           if (error) {
