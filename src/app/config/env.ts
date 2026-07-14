68f2eb0aeb536262609f9d1576f6ebce407ea6e9
Tanvir Bhuiyan <tanvirbhuiyan2001@gmail.com>
2026-07-14 07:54:44 +0600

    add JWT authentication utilities and session management middleware

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-// import status from "http-status";
-// import AppError from "../errorHelpers/AppError";
+import AppError from "../errorHelpers/AppError";
+import status from "http-status";
 
 dotenv.config();
 
@@ -35,11 +35,10 @@ const loadEnvVariables = (): EnvConfig => {
 
   requireEnvVariable.forEach((variable) => {
     if (!process.env[variable]) {
-      // throw new Error(`Environment variable ${variable} is required but not set in .env file.`);
-      //   throw new AppError(
-      //     status.INTERNAL_SERVER_ERROR,
-      //     `Environment variable ${variable} is required but not set in .env file.`,
-      //   );
+      throw new AppError(
+        status.INTERNAL_SERVER_ERROR,
+        `Environment variable ${variable} is required but not set in .env file.`,
+      );
     }
   });
 
